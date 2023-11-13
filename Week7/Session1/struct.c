4c2b85c3e9a3cd27521321c5f8c3a91c77ad81d9
Arash-BozorgChenani <145478046+Arash-BozorgChenani@users.noreply.github.com>
2023-11-13 13:31:19 +0000

    Week7Session1

---
+++ Week7/Session1/struct.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    typedef struct
+  {
+   char date[20];
+   float bloodIron;
+  } reading;
+
+
+ // an instance of the struct
+ reading Today;
+ Today.bloodIron=13;
+
+ // an array of the struct
+ reading Week[7];
+ Week[0].bloodIron=5;
+ 
+ strcpy(Week[0].date,"Example");
+
+}
